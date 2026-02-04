63d3e4c31bc9fdc986fc9936d4322b73e95326a6
DarkWizard5513 <humzaj002@gmail.com>
2026-02-04 18:49:05 +0000

    Shifted program to database instead of csv file. Limited functionality, adding and listing tasks.

--- main.py
+++ main.py
@@ -1,31 +1,48 @@
 from sys import argv, exit
+import sqlite3
 
 
-def add_task(task):
-    with open("tasks.csv", "a") as file:
-        file.write(f"{task},F\n")
-        
-
-def list_tasks():
-    counter = 1
-    with open("tasks.csv", "r") as file:
-        line = file.readline().strip("\n")
-        while line:
-            items = line.split(",")
-            if items[1] == "F":
-                print(f"[ ] {counter} {items[0]}")
-            else:
-                print(f"[X] {counter} {items[0]}")
-
-            line = file.readline().strip("\n")
-            counter += 1
+def create_table(conn, cursor):
+    cursor.execute("""
+CREATE TABLE IF NOT EXISTS tasks (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    description TEXT NOT NULL,
+    completed INTEGER NOT NULL
+);
+    """)
+    conn.commit()
     
 
+def add_task(conn, cursor, task):
+    cursor.execute("INSERT INTO tasks(description, completed) VALUES(?, ?)", (task, 0))
+    conn.commit()
+
+
+def list_tasks(cursor):
+    cursor.execute("SELECT * FROM tasks")
+    results = cursor.fetchall()
+    for result in results:
+        if result[2] == 0:
+            print(f"[ ] {result[0]} {result[1]}")
+        else:
+            print(f"[X] {result[0]} {result[1]}")
+
+try:
+    conn = sqlite3.connect("list.db")
+except:
+    print("Error connecting to database.")
+    exit()
+else:
+    cursor = conn.cursor()
+    create_table(conn, cursor)
+
+
 if argv[1] == "add":
-    add_task(argv[2])
+    add_task(conn, cursor, argv[2])
 
 elif argv[1] == "list":
-    list_tasks()
+    list_tasks(cursor)
 
 else:
+    conn.close()
     exit()
